e021ea751b20a4a032edb4e8822289ec5c58e9d0
zach <zaspringer@gmail.com>
2019-07-17 15:31:39 -0600

    added basic auth protection on the dashboard

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const massive = require('massive');
 const session = require('express-session');
 const ac = require('./controllers/authController');
+const dc = require('./controllers/dashController');
 const initSession = require('./middleware/initSession');
 const authCheck = require('./middleware/authCheck');
 const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env;
@@ -24,5 +25,6 @@ app.use(initSession);
 
 app.post('/api/login', ac.login);
 app.post('/api/register', ac.register);
+app.get('/api/dashboard', authCheck, dc.getDashboard);
 
 app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`));
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 
-export default function Dashboard(props) {
-  return <div>Dashboard</div>;
+class Dashboard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      loading: true,
+      user: {},
+      redirect: false
+    };
+  }
+  async componentDidMount() {
+    try {
+      let { data } = await axios.get('/api/dashboard');
+      this.setState({ user: data, loading: false });
+    } catch (err) {
+      this.setState({ loading: false, redirect: true });
+    }
+  }
+
+  render() {
+    let { loading, user, redirect } = this.state;
+    if (loading) return <div>Loading</div>;
+    if (redirect) return <Redirect to="/login" />;
+    return <div>Dashboard</div>;
+  }
 }
+
+export default Dashboard;
